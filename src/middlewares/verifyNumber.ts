75c051d8966b5455739d9043da7b89d132f5d898
Pablo Pessanha <pablopessanh@gmail.com>
2021-09-05 20:51:41 -0300

    fix: change req.params to req.query

--- src/middlewares/verifyNumber.ts
+++ src/middlewares/verifyNumber.ts
@@ -1,7 +1,7 @@
 import { Request, Response, NextFunction } from 'express';
 
 export default (req: Request, res: Response, next: NextFunction) => {
-  const days = Number(req.params.days);
+  const days = Number(req.query.days);
   if (days <= 0 || !Number.isInteger(days) || Number.isNaN(days)) {
     return res.status(400).json({ error: 'Days must be integer positive number' });
   }
