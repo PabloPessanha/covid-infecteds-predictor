88ceb06cc541c0c8e71741e0d119b13a41bd223d
Pablo Pessanha <pablopessanh@gmail.com>
2021-09-05 18:24:39 -0300

    feat: add route to receive days by query params

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,24 @@
-import express from 'express';
+import express, { Request, Response } from 'express';
+import updateLocalData from './data/updateLocalData';
+import verifyNumber from './middlewares/verifyNumber';
+// import CovidController from './controllers/CovidController';
 
 const app = express();
 
 app.use(express.json());
 
-app.get('/', (req, res) => res.status(200).json({ message: 'ok' }));
+app.get('/predict', verifyNumber, (req: Request, res: Response) => {
+  const { days } = req.query;
+  return res.status(200).json({ days });
+});
+
+app.get('/update', async (req, res) => {
+  try {
+    await updateLocalData();
+    return res.status(201).json({ message: 'updated!' });
+  } catch ({ message }) {
+    return res.status(400).json({ error: message });
+  }
+});
 
 app.listen(3333);
