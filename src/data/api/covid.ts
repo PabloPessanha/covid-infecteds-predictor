f67ddaf70a725a2d7b3ca7553b36d52066fc164b
Pablo Pessanha <pablopessanh@gmail.com>
2021-09-04 22:28:22 -0300

    feat: fetch covid data world infection history

---
+++ src/data/api/covid.ts
@@ -0,0 +1,11 @@
+import axios from 'axios';
+import { CovidDataResponse } from './covid.d';
+
+export default async function getCovidData(): Promise<CovidDataResponse> {
+  const { data: convidInfos } = await axios.get(
+    'https://covid.ourworldindata.org/data/owid-covid-data.json',
+  );
+  const { data } = convidInfos.OWID_WRL;
+
+  return data;
+}
