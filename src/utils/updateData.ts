ed067737cf9228e6c24569b9f2ae550515ef6d6e
Pablo Pessanha <pablopessanh@gmail.com>
2021-09-05 20:53:06 -0300

    feat: add new function to update infos every day

---
+++ src/utils/updateData.ts
@@ -0,0 +1,27 @@
+import fs from 'fs';
+import { resolve } from 'path';
+import getCovidData from '../data/api/covid';
+
+export async function formatCovidData() {
+  const { data } = await getCovidData();
+  return data.map(({ new_cases }, index) => ({
+    x: index + 1,
+    y: new_cases,
+  }));
+}
+
+export async function updateLocalData() {
+  const worldwideFile = resolve(__dirname, '..', 'data', 'worldwide-infected.json');
+  const data = await formatCovidData();
+  fs.writeFileSync(worldwideFile, JSON.stringify(data));
+}
+
+export function updateEveryDay() {
+  const minute = 1000 * 60;
+  const hour = minute * 60;
+  const day = hour * 24;
+
+  setInterval(() => {
+    updateLocalData();
+  }, day);
+}
