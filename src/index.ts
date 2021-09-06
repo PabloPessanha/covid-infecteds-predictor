9c7e7913acfedc27f92cdb42a59b676834e9036b
Pablo Pessanha <pablopessanh@gmail.com>
2021-09-05 21:07:20 -0300

    refactor: update routes

--- src/index.ts
+++ src/index.ts
@@ -1,24 +1,16 @@
-import express, { Request, Response } from 'express';
-import updateLocalData from './data/updateLocalData';
+import express from 'express';
 import verifyNumber from './middlewares/verifyNumber';
-// import CovidController from './controllers/CovidController';
+import PredictController from './controllers/PredictController';
+import { updateEveryDay } from './utils/updateData';
 
 const app = express();
 
 app.use(express.json());
 
-app.get('/predict', verifyNumber, (req: Request, res: Response) => {
-  const { days } = req.query;
-  return res.status(200).json({ days });
-});
+app.get('/data', PredictController.data);
 
-app.get('/update', async (req, res) => {
-  try {
-    await updateLocalData();
-    return res.status(201).json({ message: 'updated!' });
-  } catch ({ message }) {
-    return res.status(400).json({ error: message });
-  }
-});
+app.get('/predict', verifyNumber, PredictController.predict);
+
+updateEveryDay();
 
 app.listen(3333);
