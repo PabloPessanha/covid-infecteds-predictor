17b9856935a64ef0444273008a6048ce1dc24ec1
Pablo Pessanha <pablopessanh@gmail.com>
2021-09-06 09:06:06 -0300

    chore: remove data folder

--- src/data/api/covid.ts
+++
@@ -1,9 +0,0 @@
-import axios from 'axios';
-import { CovidDataResponse } from './covid.d';
-
-export default async function getCovidData(): Promise<CovidDataResponse> {
-  const { data: convidInfos } = await axios.get(
-    'https://covid.ourworldindata.org/data/owid-covid-data.json',
-  );
-  return convidInfos.OWID_WRL;
-}
